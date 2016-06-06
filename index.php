b7e088850bf8303c55f3c85125a269e9714bdf44
三浦 <k014c1138@it-neec.jp>
2016-06-06 10:45:54 +0900

    add a line

--- index.php
+++ index.php
@@ -6,6 +6,9 @@
 </head>
 
 <body>
+
+
+
   <?php
  try{
   $dsn = 'mysql:dbname=todolist;host=localhost;charset=utf8';
@@ -13,57 +16,55 @@
   $password = 'root';
   $dbh = new PDO($dsn, $user);
   $dbh->query('SET NAMES utf8');
-  $dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, false)
-  if(isset($_GET['add'])){
-    $item = $_GET['item'];
-    $item = htmlspecialchars($item, ENT_QUOTES);
-    $error = "";
-    if($item === "") {
-      $error = "メモが入力されていません。";
-    } else{
-      $sql = 'INSERT INTO　list (item) VALUES (:item)';
-      $stmt = $dbh->prepare($sql);
-      $stmt->bindParam(':item', $item);
-      $stmt->execute();
-    }
-    unset($item);
+  $dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+
+
+
+
+  if(isset($_GET['add'])) {
+
+    $sql = 'INSERT INTO list(item) VALUES(?)';
+    $stmt = $dbh->prepare($sql);
+    $data[] = $_GET['name'];
+    $stmt-> execute($data);
+
   } else if(isset($_GET['delete'])) {
     $sql = 'DELETE FROM list WHERE id=?';
     $stmt = $dbh->prepare($sql);
-    $data[] = $_GET['name'];
+    $data[] = $_GET['id'];
     $stmt->execute($data);
 
-    $dbh = null;
   }
-  $sql = 'SELECT item FROM list';
+  $sql = 'SELECT id, item FROM list';
   $stmt = $dbh->prepare($sql);
   $stmt-> execute();
 
-  $dbh = null;
   ?>
 <form method = "get" action = "index.php">
 <input type="text" name = "name" style="width:200px">
-<input type="submit" value="add"><br />
+<input type="submit" name = "add" value="add"><br />
 </form>
 
-<form method = "get" action = "project_delete">
 <?php
 while(true) {
   $rec = $stmt->fetch(PDO::FETCH_ASSOC);
   if($rec==false){
+    $dbh = null;
     break;
   }
-  print $rec['item'];
   ?>
-  <input type="submit" value="delete" name="$rec['id']" style="background:url('submit_delete.jpg')" />
-  <?php
-  print '</br>';
-}
+  <form method = "get" action = "index.php">
+  <?php print $rec['item']; ?>
+  <input type ="hidden" name="id" value="<?php print $rec['id'] ?>" >
+  <input type="submit"  name="delete"  value="delete">
+  <?php print '</br>'; ?>
+</form>
+
+<?php }
 } catch(Exception $e) {
  print'エラー';
  exit();
-}
-  ?>
+} ?>
 </body>
 
 </html>
